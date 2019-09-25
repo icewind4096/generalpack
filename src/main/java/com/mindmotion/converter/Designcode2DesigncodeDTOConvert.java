87d1396c7170b49b4c1fa94b7dbe9b00db84b6cc
iceind4096 <icewind4096@gmail.com>
2019-09-25 23:07:02 +0800

    Designcode数据库实体对象转换为传输对象

---
+++ src/main/java/com/mindmotion/converter/Designcode2DesigncodeDTOConvert.java
@@ -0,0 +1,32 @@
+package com.mindmotion.converter;
+
+import com.mindmotion.domain.Designcode;
+import com.mindmotion.dto.DesigncodeDTO;
+import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by mecwa on 2019/9/25.
+ */
+public class Designcode2DesigncodeDTOConvert {
+    public static DesigncodeDTO convert(Designcode designcode){
+        DesigncodeDTO designcodeDTO = new DesigncodeDTO();
+
+        BeanUtils.copyProperties(designcode, designcodeDTO);
+
+        return designcodeDTO;
+    }
+
+    public static List<DesigncodeDTO> convert(List<Designcode> designcodeList) {
+        List<DesigncodeDTO> designcodeDTOList = new ArrayList<>();
+        for (Designcode designcode:designcodeList){
+            DesigncodeDTO designcodeDTO = new DesigncodeDTO();
+            BeanUtils.copyProperties(designcode, designcodeDTO);
+            designcodeDTOList.add(designcodeDTO);
+        }
+        return designcodeDTOList;
+    }
+}
