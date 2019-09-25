cad5f6224ae631ee4cd595a7cb227ef3ae91470f
iceind4096 <icewind4096@gmail.com>
2019-09-25 13:48:13 +0800

    1. 定义DesigncodeDTO, 用做Designcode的数据传输

---
+++ src/main/java/com/mindmotion/dto/DesigncodeDTO.java
@@ -0,0 +1,35 @@
+package com.mindmotion.dto;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by mecwa on 2019/9/25.
+ */
+public class DesigncodeDTO {
+    private Integer id;
+    private String code;
+    private String corename;
+    private String corever;
+    private Integer minfreq;
+    private Integer fpu;
+    private Integer mpu;
+    private Integer simd;
+    private Integer jtag;
+    private Integer swd;
+    private Integer swotraced0;
+    private Integer endian;
+    private Integer thumbsupport;
+    private Integer armsupport;
+    private Integer flashbase;
+    private Integer rambase;
+    private Integer intvecbase;
+    private Integer block;
+    private Integer page;
+    private Integer trusterange;
+    private Integer usesfrfilter;
+    private Integer online;
+    private Integer aggregate;
+    private String argsdoc;
+    private String ddfname;
+    private Timestamp crtime;
+}
