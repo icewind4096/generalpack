4d2aa627f3c9aa77213dd274be66f4870b945a38
iceind4096 <icewind4096@gmail.com>
2019-09-25 22:22:26 +0800

    Key生成类

---
+++ src/main/java/com/mindmotion/utils/KeyUtil.java
@@ -0,0 +1,22 @@
+package com.mindmotion.utils;
+
+import java.util.Random;
+
+/**
+ * Created by mecwa on 2019/9/25.
+ */
+public class KeyUtil {
+    /*
+    生成主键
+    格式时间+随机数
+     */
+    public static String genUniqueKey(){
+        Random random = new Random();
+
+        System.currentTimeMillis();
+
+        Integer value = random.nextInt(900000) + 100000;
+
+        return System.currentTimeMillis() + String.valueOf(value);
+    }
+}
