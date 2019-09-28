86e44d151c010017670f1039c9b5c59c9af6cfa6
iceind4096 <icewind4096@gmail.com>
2019-09-28 12:53:36 +0800

    添加saveToFile方法

--- src/main/java/com/mindmotion/utils/FileUtils.java
+++ src/main/java/com/mindmotion/utils/FileUtils.java
@@ -1,6 +1,9 @@
 package com.mindmotion.utils;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by mecwa on 2019/9/26.
@@ -10,6 +13,20 @@ public class FileUtils {
         File directory = new File(directoryName);
         if (directory.exists() == false){
             directory.mkdirs();
+        }
+    }
+
+    public static boolean saveToFile(String fileName, StringBuilder stringBuilder){
+        File file = new File(fileName);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(stringBuilder.toString());
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
