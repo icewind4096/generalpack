0de65937081f97705d68cd96f748c3b1e6616681
iceind4096 <icewind4096@gmail.com>
2019-09-28 14:55:37 +0800

    Part到PartDTO的转换

--- src/main/java/com/mindmotion/converter/Part2PartDTOConvert.java
+++ src/main/java/com/mindmotion/converter/Part2PartDTOConvert.java
@@ -1,7 +1,18 @@
 package com.mindmotion.converter;
 
+import com.mindmotion.domain.Part;
+import com.mindmotion.dto.PartDTO;
+import org.springframework.beans.BeanUtils;
+
 /**
  * Created by mecwa on 2019/9/28.
  */
 public class Part2PartDTOConvert {
+    public static PartDTO convert(Part part){
+        PartDTO partDTO = new PartDTO();
+
+        BeanUtils.copyProperties(part, partDTO);
+
+        return partDTO;
+    }
 }
