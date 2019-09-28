c1873cd7ab0dbf774f64af9c4b164ccb88e1933c
iceind4096 <icewind4096@gmail.com>
2019-09-28 14:55:05 +0800

    添加一个自定义级联查找，part->family->designcode

--- src/main/java/com/mindmotion/dao/DesigncodeDAO.java
+++ src/main/java/com/mindmotion/dao/DesigncodeDAO.java
@@ -4,6 +4,8 @@ import com.mindmotion.domain.Designcode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by mecwa on 2019/9/22.
@@ -12,4 +14,7 @@ public interface DesigncodeDAO extends JpaRepository<Designcode, Integer>{
     Designcode findByCode(String code);
 
     Page<Designcode> findAll(Pageable pageable);
+
+    @Query(nativeQuery=true, value = "select a.* from designcode a where a.code in (select designcode from partfamily b where b.familyname in (select familyname from part where partname = :partName))")
+    Designcode findByPartNameParam(@Param("partName") String partName);
 }
