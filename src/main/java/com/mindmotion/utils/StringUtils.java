f03b5ed099d9876dd922c0f444d60f13b90433a5
icewind4096 <icewind4096@gmail.com>
2019-09-29 16:19:00 +0800

    1. 修改函数名Int2BoolString，改为int2BoolString 2. 添加函数int2HexString

--- src/main/java/com/mindmotion/utils/StringUtils.java
+++ src/main/java/com/mindmotion/utils/StringUtils.java
@@ -4,11 +4,26 @@ package com.mindmotion.utils;
  * Created by mecwa on 2019/9/26.
  */
 public class StringUtils {
-    public static String Int2BoolString(Integer value){
+    public static String int2BoolString(Integer value){
         if (value == 0){
             return "false";
         } else {
             return "true";
         }
+    }
+
+    public static String int2HexString(Integer value, Integer width, Boolean preEn, Boolean posEn, Boolean lowCase){
+        String exp = "%0" + String.valueOf(width) + "x";
+        exp = String.format(exp, value);
+        if (preEn == true) {
+            exp = "0x" + exp;
+        }
+        if (posEn == true){
+            exp = exp + "h";
+        }
+        if (lowCase == false){
+            exp = exp.toUpperCase();
+        }
+        return exp;
     }
 }
