52b55273a89987cb0c4c4388cdaeb710426b2443
iceind4096 <icewind4096@gmail.com>
2019-09-29 22:45:10 +0800

    初步完成新建Desgincode流程

--- src/main/java/com/mindmotion/controller/DesignCodeController.java
+++ src/main/java/com/mindmotion/controller/DesignCodeController.java
@@ -1,23 +1,30 @@
 package com.mindmotion.controller;
 
+import com.mindmotion.converter.DesigncodeForm2DesigncodeDTO;
 import com.mindmotion.domain.Designcode;
 import com.mindmotion.dto.DesigncodeDTO;
 import com.mindmotion.enums.ResultEnum;
+import com.mindmotion.exception.GeneratePackException;
+import com.mindmotion.form.DesigncodeForm;
 import com.mindmotion.service.DesigncodeService;
 import com.mindmotion.utils.ResultVOUtil;
 import com.mindmotion.vo.DesigncodeVO;
 import com.mindmotion.vo.ResultVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.xml.transform.Result;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * DesignCode
@@ -26,9 +33,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/designcode")
+@Slf4j
 public class DesignCodeController {
     @Autowired
     DesigncodeService designcodeService;
+
+    public ResultVO create(@Valid DesigncodeForm designcodeForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            log.error("[创建Desigcode]->参数不正确， orderForm={}", designcodeForm);
+            throw new GeneratePackException(ResultEnum.PARMATER_ERROR.getCode()
+                                           ,bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        DesigncodeDTO designcodeDTO = DesigncodeForm2DesigncodeDTO.convert(designcodeForm);
+        designcodeService.create(designcodeDTO);
+
+        return ResultVOUtil.success();
+    }
 
     @GetMapping("/list")
     public ResultVO List(){
