978b4ec5916a5ec9a522aa4a53938294792c734a
icewind4096 <icewind4096@gmail.com>
2019-10-08 17:57:15 +0800

    完成产生全部IARPack

--- src/test/java/com/mindmotion/service/impl/GeneralPackServiceImplTest.java
+++ src/test/java/com/mindmotion/service/impl/GeneralPackServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.mindmotion.service.impl;
 
 import com.mindmotion.service.GeneralPackService;
+import com.mindmotion.utils.FileUtils;
+import com.mindmotion.utils.ZipCompressFile;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +12,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ResourceUtils;
+
+import java.io.File;
 
 import static org.junit.Assert.*;
 
@@ -23,9 +27,22 @@ public class GeneralPackServiceImplTest {
     GeneralPackServiceImpl generalPackService;
 
     @Test
-    public void generalIARPack() throws Exception {
+    public void generateIARPackByPartNameTest() throws Exception {
         Resource resource = new ClassPathResource("");
-        Integer value = generalPackService.generateIARPackByPartName(resource.getURI().getPath() + "\\iartpack\\","MM32F032K6U6");
+        String rootDirectory = resource.getURI().getPath() + "\\MM32_EWARM_Pack\\";
+        Integer value = generalPackService.generateIARPackByPartName(rootDirectory,"MM32F032K6U6");
+        FileUtils.zipDirectory(String.format("%s\\MM32_EWARM_Pack.zip", FileUtils.getParentDirectory(rootDirectory)), rootDirectory);
+        FileUtils.delDirectorys(rootDirectory);
+        Assert.assertEquals(Integer.valueOf(0), value);
+    }
+
+    @Test
+    public void generateIARPackAllTest() throws Exception {
+        Resource resource = new ClassPathResource("");
+        String rootDirectory = resource.getURI().getPath() + "\\MM32_EWARM_Pack\\";
+        Integer value = generalPackService.generateIARPackAll(rootDirectory);
+        FileUtils.zipDirectory(String.format("%s\\MM32_EWARM_Pack.zip", FileUtils.getParentDirectory(rootDirectory)), rootDirectory);
+        FileUtils.delDirectorys(rootDirectory);
         Assert.assertEquals(Integer.valueOf(0), value);
     }
 
