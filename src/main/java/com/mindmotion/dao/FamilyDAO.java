3c649deee5794df94f697037ed1698de58b91de7
icewind4096 <icewind4096@gmail.com>
2019-12-13 10:58:44 +0800

    1. 删除findByFamilyname查询函数 2. 添加findByFamilynameParam查询函数

--- src/main/java/com/mindmotion/dao/FamilyDAO.java
+++ src/main/java/com/mindmotion/dao/FamilyDAO.java
@@ -2,6 +2,8 @@ package com.mindmotion.dao;
 
 import com.mindmotion.domain.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,7 +11,9 @@ import java.util.List;
  * Created by MMNJ002 on 2019/10/10.
  */
 public interface FamilyDAO extends JpaRepository<Family, Integer> {
-    Family findByFamilyname(String familyName);
     List<Family> findAllByOrderByIdAsc();
     List<Family> findAllByParentidOrderById(Integer parentid);
+
+    @Query(nativeQuery=true, value = "select * from family where (parentid = -1) and (familyname = :familyName)")
+    Family findByFamilynameParam(@Param("familyName") String familyName);
 }
